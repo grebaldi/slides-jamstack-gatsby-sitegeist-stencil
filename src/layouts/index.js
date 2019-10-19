981e749bca78c59fbd2a091c0ef35274d2c1d181
Wilhelm Behncke <wilhelm.behncke@protonmail.com>
2019-10-19 14:23:42 +0200

    Add basic styles

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,108 +8,121 @@ import Transition from '../components/transition';
 import './index.css';
 
 const Header = ({ name, title, date }) => (
-  <header>
-    <Link to="/1">
-      <span>{name}</span> — {title}
-    </Link>
-    <time>{date}</time>
-  </header>
+	<header>
+		<Link to="/1">
+			<span>{name}</span> — {title}
+		</Link>
+		<time>{date}</time>
+	</header>
 );
 
 class TemplateWrapper extends Component {
-  NEXT = [13, 32, 39];
-  PREV = 37;
+	NEXT = [13, 32, 39];
+	PREV = 37;
 
-  swipeLeft = () => {
-    this.navigate({ keyCode: this.NEXT[0] });
-  };
+	swipeLeft = () => {
+		this.navigate({ keyCode: this.NEXT[0] });
+	};
 
-  swipeRight = () => {
-    this.navigate({ keyCode: this.PREV });
-  };
+	swipeRight = () => {
+		this.navigate({ keyCode: this.PREV });
+	};
 
-  navigate = ({ keyCode }) => {
-    const now = this.props.data.slide.index;
-    const slidesLength = this.props.slidesLength;
+	navigate = ({ keyCode }) => {
+		const now = this.props.data.slide.index;
+		const slidesLength = this.props.slidesLength;
 
-    if (now) {
-      if (keyCode === this.PREV && now === 1) {
-        return false;
-      } else if (this.NEXT.indexOf(keyCode) !== -1 && now === slidesLength) {
-        return false;
-      } else if (this.NEXT.indexOf(keyCode) !== -1) {
-        navigate(`/${now + 1}`);
-      } else if (keyCode === this.PREV) {
-        navigate(`/${now - 1}`);
-      }
-    }
-  };
+		if (now) {
+			if (keyCode === this.PREV && now === 1) {
+				return false;
+			} else if (this.NEXT.indexOf(keyCode) !== -1 && now === slidesLength) {
+				return false;
+			} else if (this.NEXT.indexOf(keyCode) !== -1) {
+				navigate(`/${now + 1}`);
+			} else if (keyCode === this.PREV) {
+				navigate(`/${now - 1}`);
+			}
+		}
+	};
 
-  componentDidMount() {
-    document.addEventListener('keydown', this.navigate);
-  }
+	componentDidMount() {
+		document.addEventListener('keydown', this.navigate);
+	}
 
-  componentWillUnmount() {
-    document.removeEventListener('keydown', this.navigate);
-  }
+	componentWillUnmount() {
+		document.removeEventListener('keydown', this.navigate);
+	}
 
-  render() {
-    const { location, children, site } = this.props;
+	render() {
+		const { location, children, site, logo } = this.props;
 
-    return (
-      <div>
-        <Helmet
-          title={`${site.siteMetadata.title} — ${site.siteMetadata.name}`}
-        />
-        <Header
-          name={site.siteMetadata.name}
-          title={site.siteMetadata.title}
-          date={site.siteMetadata.date}
-        />
-        <Swipeable
-          onSwipedLeft={this.swipeLeft}
-          onSwipedRight={this.swipeRight}
-        >
-          <Transition location={location}>
-            <div id="slide" style={{'width': '100%'}}>{children}</div>
-          </Transition>
-        </Swipeable>
-      </div>
-    );
-  }
+		return (
+			<div>
+				<Helmet
+					title={`${site.siteMetadata.title} — ${site.siteMetadata.name}`}
+				/>
+				<Header
+					name={site.siteMetadata.name}
+					title={site.siteMetadata.title}
+					date={site.siteMetadata.date}
+				/>
+				<Swipeable
+					onSwipedLeft={this.swipeLeft}
+					onSwipedRight={this.swipeRight}
+				>
+					<Transition location={location}>
+						<div id="slide" style={{ width: '100%' }}>
+							{children}
+						</div>
+					</Transition>
+				</Swipeable>
+				<footer>
+					<img src={logo} alt="Sitegeist Logo" />
+				</footer>
+			</div>
+		);
+	}
 }
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.node,
-  data: PropTypes.object,
+	children: PropTypes.node,
+	data: PropTypes.object,
 };
 
 export default props => (
-  <StaticQuery
-    query={graphql`
-      query IndexQuery {
-        site {
-          siteMetadata {
-            name
-            title
-            date
-          }
-        }
-        allSlide {
-          edges {
-            node {
-              id
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <TemplateWrapper
-        site={data.site}
-        slidesLength={data.allSlide.edges.length}
-        {...props}
-      />
-    )}
-  />
+	<StaticQuery
+		query={graphql`
+			query IndexQuery {
+				site {
+					siteMetadata {
+						name
+						title
+						date
+					}
+				}
+				allSlide {
+					edges {
+						node {
+							id
+						}
+					}
+				}
+				allFile(filter: {relativePath: {eq: "logo-sitegeist-bright.png"}}) {
+					edges {
+						node {
+							publicURL
+						}
+					}
+				}
+			}
+		`}
+		render={data => (
+			<TemplateWrapper
+				site={data.site}
+				logo={data.allFile.edges[0].node.publicURL}
+				slidesLength={data.allSlide.edges.length}
+				{...props}
+			/>
+		)}
+	/>
 );
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { navigate } from 'gatsby';
 
 class Index extends Component {
-  componentDidMount() {
-    navigate(`/1`, { replace: true });
-  }
-  render() {
-    return <div />;
-  }
+	componentDidMount() {
+		navigate(`/1`, { replace: true });
+	}
+
+	render() {
+		return <div />;
+	}
 }
 
 export default Index;
